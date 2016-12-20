3013d3f652be8a29c8dc1fa189e5ce4f5764f217
jacopo <jacopom@ifi.uio.no>
2016-12-20 12:47:39 +0100

    update dockerfile, add benchmark script

--- docker/Dockerfile.m4
+++ docker/Dockerfile.m4
@@ -3,37 +3,37 @@ MAINTAINER Jacopo Mauro
 
 # download and install zephyurs2
 RUN cd / && \
-	mkdir solvers_exec && \
-  cd /solvers_exec && \
-  git clone --recursive -b bind_preferences https://jacopomauro@bitbucket.org/jacopomauro/zephyrus2.git && \
-	cd zephyrus2 && \
-	git checkout 924b50f04c73b8269d3b14157dd0abbf7b5bd99c && \ 
-	#check out tested version with smartdeployer
-  pip install -e /solvers_exec/zephyrus2
+    mkdir solvers_exec && \
+    cd /solvers_exec && \
+    git clone --recursive --depth=1 https://jacopomauro@bitbucket.org/jacopomauro/zephyrus2.git && \
+    cd zephyrus2 && \
+    pip install -e /solvers_exec/zephyrus2
 
 include(`MiniZincIDE_binary.m4')
 include(`gecode_binary.m4')
+include(`fzn2smt.m4')
 include(`chuffed.m4')
+include(`z3.m4')
 
 # clone abs_deployer
 RUN cd /solvers_exec && \
-	git clone --recursive --depth=1 -b bind_pref https://github.com/jacopoMauro/abs_deployer.git
+	git clone --recursive --depth=1 https://github.com/jacopoMauro/abs_deployer.git
 
 ENV PATH /solvers_exec/abs_deployer:$PATH
 
 # install abs-tools
 RUN cd /solvers_exec && \
 	apt-get update && \
-  apt-get install -y \
+    apt-get install -y \
 		openjdk-7-jdk \
 		ant \
 		cmake \
 		bison \
 		flex && \
-  rm -rf /var/lib/apt/lists/* && \
+    rm -rf /var/lib/apt/lists/* && \
 	cd /solvers_exec && \
 	git clone --depth=1 https://github.com/abstools/abstools.git && \
-  cd /solvers_exec/abstools/frontend && ant
+    cd /solvers_exec/abstools/frontend && ant
 
 ENV CLASSPATH=/solvers_exec/abstools/frontend/dist/absfrontend.jar:$CLASSPATH
 	
---
+++ test/run_benchmark.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python
+
+import json
+from subprocess import Popen,PIPE
+import uuid
+import math
+import logging as log
+import sys
+import fileinput
+import re
+import os
+
+# log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
+
+REPETITIONS = 1
+TEST_FILE = 'test_addquery.abs'
+
+# doing a grid search
+EUROPE = range(0,5)
+USA = range(0,10)
+
+TIMEOUT = 120
+
+# OTHER_OPT
+OTHER_OPT = [ ]
+OTHER_ARGS = [ 'Monitor.abs', 'Degradation.abs']
+
+CMDS = [
+          # smt
+          # ['time', '-p', 'timeout', str(TIMEOUT),
+          # 'python', '../abs_deployer.py',
+          # '-s', 'smt'],
+
+          # chuffed
+          ['time', '-p', 'timeout', str(TIMEOUT),
+          'python', '../abs_deployer.py',
+          '-s', 'chuffed'],
+
+          # gecode
+          ['time', '-p', 'timeout', str(TIMEOUT),
+          'python', '../abs_deployer.py',
+          '-s', 'gecode'],
+
+        ]
+
+
+def changefile(eu,us):
+    f_name = '/tmp/' + uuid.uuid4().hex + '.abs'
+    with open(f_name,'w') as f:
+        for line in fileinput.input(TEST_FILE):
+            line = re.sub(
+                r"\(sum\s\?x\sin\s'\.\*us\.':\s\?x\.QueryServiceImpl\['live'\]\)\s=\s[0-9]+",
+                "(sum ?x in '.*us.': ?x.QueryServiceImpl['live']) = " + unicode(us),
+                line)
+            line = re.sub(
+                r"\(sum\s\?x\sin\s'\.\*eu':\s\?x\.QueryServiceImpl\['live'\]\)\s=\s[0-9]+",
+                "(sum ?x in '.*eu': ?x.QueryServiceImpl['live']) = " + unicode(us),
+                line)
+            f.write(line)
+    return f_name
+
+
+
+def run_cmd(cmd):
+  log.info("Running command: "  + str.join(' ',cmd))
+  proc = Popen( cmd, stdout=PIPE, stderr=PIPE )
+  out, err = proc.communicate()
+  log.debug('Stdout')
+  log.debug(out)
+  log.debug('Stderr')
+  log.debug(err)
+  returncode = proc.returncode
+  log.debug('Return code = ' + str(returncode))
+  
+  if returncode != 0:
+    return -float(returncode)
+  else:
+    return float(re.search('real\s*([0-9\.]+)',err).group(1))
+
+
+def run(cmd):
+  times = []
+  errors = []
+  for _ in range(REPETITIONS):
+    time = run_cmd(cmd)
+    if time < 0.0:
+      log.debug('Timeout or error')
+      errors.append(time)
+      break
+    else:
+      times.append(time)
+  
+  if len(errors) > 0:
+    return (0,0, -errors[0])
+  else:
+    return (sum(times)/REPETITIONS, (max(times)- min(times))/2, 0)
+
+
+def myprint(res):
+  if res[2] > 0:
+    return("error" + str(res[2]) + " & error" + str(res[2]))
+  else:
+    return(str(res[0]) + ' & ' + str(res[1]))
+ 
+def main():
+  
+  # print first line of the table
+  print 'eu & usa',
+  for i in CMDS:
+    for j in range(len(i)):
+      if i[j] == '-s':
+        s = i[j+1]
+        break
+    print '& ' + s + ' & err',
+  print '\\\\'
+
+  # start performing the grid search
+  for eu in EUROPE:
+    for us in USA:
+          
+        # print the parameters of wordpress tested
+        print str(eu) + ' & ' + str(us) + ' & ',
+
+        # create the input file for zephyrus
+        f = changefile(eu,us)
+        log.debug('File ' + f + " generated for testing combination " + unicode((eu,us)))
+
+        # run the solvers
+        for i in range(len(CMDS)):
+            cmd = CMDS[i] + OTHER_OPT + [ f ] + OTHER_ARGS
+            res = run(cmd)
+            print myprint(res),
+            sys.stdout.flush()
+        print '\\\\'
+
+        # remove file
+        if os.path.exists(f):
+            os.remove(f)
+
+
+
+main()   
