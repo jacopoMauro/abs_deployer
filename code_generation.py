7da53418c89400def4c55201c8080cfa3756bcc7
jacopo <jacopom@ifi.uio.no>
2017-06-09 14:36:51 +0200

    remove leftover code generation

--- code_generation.py
+++ code_generation.py
@@ -172,11 +172,9 @@ def print_list_and_get_methods(interfaces,dc_json,out):
   out.write("\t\t\tif (ls_ls_DeploymentComponent != Nil) {\n")
   for i in interfaces:
     out.write("\t\t\t\tls_" + i + " = head(ls_ls_" + i + ");\n")
-  out.write("\t\t\t\tls_DeploymentComponent = head(ls_ls_DeploymentComponent);\n")
   out.write("\t\t\t} else {\n")
   for i in interfaces:
     out.write("\t\t\t\tls_" + i + " = Nil;\n")
-  out.write("\t\t\t\tls_DeploymentComponent = Nil;\n")
   out.write("\t\t\t}\n")
   out.write("\t\t}\n")
   out.write("\t}\n\n")
